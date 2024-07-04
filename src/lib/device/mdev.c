398540ea612a0791ee0086ad8b7de8eb5e2696d1
Sergey Kostanbaev <sergey.kostanbaev@gmail.com>
2024-07-04 12:53:21 +0400

    mdev: fix resource leak if initialization failed

--- src/lib/device/mdev.c
+++ src/lib/device/mdev.c
@@ -506,11 +506,19 @@ void _mdev_destroy(device_t *udev)
 int mdev_create(unsigned pcnt, const char** names, const char** values, lldev_t* odev,
                 unsigned idx, char** bus_names, unsigned bus_cnt)
 {
-    dev_multi_t* obj = (dev_multi_t*)malloc(sizeof(dev_multi_t));
+    dev_multi_t* obj;
     int res;
     unsigned i;
     const uint8_t* uuid_master;
 
+    if (bus_cnt == 0 || bus_cnt > DEV_MAX) {
+        return -EINVAL;
+    }
+
+    obj = (dev_multi_t*)malloc(sizeof(dev_multi_t));
+    if (obj == NULL) {
+        return -ENOMEM;
+    }
     memset(obj, 0, sizeof(*obj));
 
     // Creating sub-device
@@ -522,7 +530,7 @@ int mdev_create(unsigned pcnt, const char** names, const char** values, lldev_t*
 
         res = lowlevel_create(pcnt, names, values, &obj->real[i], 0, NULL, 0);
         if (res)
-            return res;
+            goto failed_create;
 
         if (i == 0) {
             uuid_master = lowlevel_get_uuid(obj->real[i]);
@@ -545,12 +553,12 @@ int mdev_create(unsigned pcnt, const char** names, const char** values, lldev_t*
     obj->lldev.ops = &s_mdev_ops;
 
     // Create virtual device
-    res = usdr_device_base_create(&obj->virt_dev, &obj->lldev);
-    if (res)
+    res = res ? res : usdr_device_base_create(&obj->virt_dev, &obj->lldev);
+    if (res) {
         goto error_init;
+    }
 
     obj->lldev.pdev = &obj->virt_dev;
-
     obj->virt_dev.destroy = &_mdev_destroy;
     obj->virt_dev.create_stream = &_mdev_create_stream;
     obj->virt_dev.unregister_stream = &_mdev_unregister_stream;
@@ -558,15 +566,24 @@ int mdev_create(unsigned pcnt, const char** names, const char** values, lldev_t*
     obj->virt_dev.vfs_get_single_object = &_mdev_get_obj;
 
     // Set multi dev for master node
-    res = usdr_device_vfs_obj_val_set_by_path(obj->real[0]->pdev, "/ll/mdev", (uintptr_t)&obj->lldev);
+    res = res ? res : usdr_device_vfs_obj_val_set_by_path(obj->real[0]->pdev, "/ll/mdev", (uintptr_t)&obj->lldev);
+    if (res) {
+        goto error_init;
+    }
 
     USDR_LOG("DSTR", USDR_LOG_WARNING, "Creating multi device with %d nodes, each %d/%d RX/TX chans, MDEV captured: %d\n",
              obj->cnt, obj->rx_chans, obj->tx_chans, res == 0 ? 1 : 0);
-
     *odev = &obj->lldev;
     return 0;
 
-
 error_init:
-    return -ENOTSUP;
+failed_create:
+    for (i = 0; i < bus_cnt; i++) {
+        if (obj->real[i]) {
+            obj->real[i]->ops->destroy(obj->real[i]);
+            obj->real[i] = NULL;
+        }
+    }
+    free(obj);
+    return res;
 }
