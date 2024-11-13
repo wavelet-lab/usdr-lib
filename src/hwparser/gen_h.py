5bdafbcde33d3a84d73904be19af107ceb6390cf
Yuri Nikolaev <irodushka@gmail.com>
2024-11-13 20:34:29 +0300

    'Not supported' exception added

--- src/hwparser/gen_h.py
+++ src/hwparser/gen_h.py
@@ -220,6 +220,8 @@ class GenH:
                     defc += ")"
                     print(defc)
                 else:
+                    raise ValueError('Not supported!')
+
                     value_msk = reduce(lambda x, y: x | y, [x.mask for x in r.fields])
                     value_off = reduce(lambda x, y: min(x, y), [x.bits_l for x in r.fields])
                     if len(r.fields) > 1:
