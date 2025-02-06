58cb126db75c7cdace4b5d588c35f47512254a59
Yuri Nikolaev <irodushka@gmail.com>
2025-02-06 20:08:48 +0300

    fix typo

--- src/lib/xdsp/conv_4ci16_ci16_2.c
+++ src/lib/xdsp/conv_4ci16_ci16_2.c
@@ -30,7 +30,7 @@ conv_function_t conv_get_4ci16_ci16_c(generic_opts_t cpu_cap, const char** sfunc
 
     SELECT_GENERIC_FN(fn, fname, tr_conv_4ci16_ci16_generic, cpu_cap);
     SELECT_AVX2_FN(fn, fname, tr_conv_4ci16_ci16_avx2, cpu_cap);
-    SELECT_NEON_FN(fn, fname, tr_conv_4ci16_ci16_avx2, cpu_cap);
+    SELECT_NEON_FN(fn, fname, tr_conv_4ci16_ci16_neon, cpu_cap);
 
     if (sfunc) *sfunc = fname;
     return fn;
