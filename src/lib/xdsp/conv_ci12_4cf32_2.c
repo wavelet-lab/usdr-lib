069f9dd96af2e65280162466dff9a59abdfd600d
Yuri Nikolaev <irodushka@gmail.com>
2025-02-20 13:48:01 +0300

    fix typo in conv_ci12_4cf32_2.c

--- src/lib/xdsp/conv_ci12_4cf32_2.c
+++ src/lib/xdsp/conv_ci12_4cf32_2.c
@@ -32,7 +32,7 @@ conv_function_t conv_get_ci12_4cf32_c(generic_opts_t cpu_cap, const char** sfunc
 
     SELECT_GENERIC_FN(fn, fname, tr_conv_ci12_4cf32_generic, cpu_cap);
     SELECT_AVX2_FN(fn, fname, tr_conv_ci12_4cf32_avx2, cpu_cap);
-    SELECT_NEON_FN(fn, fname, tr_conv_ci12_4cf32_neon, cpu_cap)
+    SELECT_NEON_FN(fn, fname, tr_conv_ci12_4cf32_neon, cpu_cap);
 
     if (sfunc) *sfunc = fname;
     return fn;
