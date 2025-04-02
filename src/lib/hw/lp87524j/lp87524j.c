bcf66b0c184ca2c49266633bdc902a8ad68f5397
Yuri Nikolaev <irodushka@gmail.com>
2025-04-02 19:27:00 +0300

    update yamls, add bus info

--- src/lib/hw/lp87524j/lp87524j.c
+++ src/lib/hw/lp87524j/lp87524j.c
@@ -1,6 +1,6 @@
 // Copyright (c) 2025 Wavelet Lab
 // SPDX-License-Identifier: MIT
 
-#include "def_lp87524j_q1.h"
-#include "lp87524j_q1.h"
+#include "def_lp87524j.h"
+#include "lp87524j.h"
 #include "usdr_logging.h"
--- src/lib/hw/lp87524j/lp87524j.h
+++ src/lib/hw/lp87524j/lp87524j.h
@@ -1,7 +1,7 @@
 // Copyright (c) 2025 Wavelet Lab
 // SPDX-License-Identifier: MIT
 
-#ifndef LP87524J_Q1_H
-#define LP87524J_Q1_H
+#ifndef LP87524J
+#define LP87524J
 
-#endif // LP87524J_Q1_H
+#endif // LP87524J
