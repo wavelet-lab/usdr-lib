064f86b7acd1466c0b4aebbfe74ca4aac64de7a4
Sergey Kostanbaev <sergey.kostanbaev@gmail.com>
2025-12-18 01:19:22 +0400

    fix dsdr

--- src/lib/device/m2_dsdr/m2_dsdr.c
+++ src/lib/device/m2_dsdr/m2_dsdr.c
@@ -775,7 +775,302 @@ static const uint16_t s_gth_usp_regs[] = {
     0x00B0,
 };
 
-#define MAKE_DSDR_DRP_CMD(wr, idx, addr, data) ((((wr) & 1) << 28) | (((idx) ? 2 : 1) << 29) | (((addr) & 0xfff) << 16) | ((data) & 0xffff))
+static const uint16_t s_gth_usp_chan_regs[] = {
+    0x0002,
+    0x0003,
+    0x0004,
+    0x0005,
+    0x0006,
+    0x0009,
+    0x000B,
+    0x000C,
+    0x000E,
+    0x000F,
+    0x0010,
+    0x0011,
+    0x0012,
+    0x0013,
+    0x0014,
+    0x0015,
+    0x0016,
+    0x0017,
+    0x0018,
+    0x0019,
+    0x001A,
+    0x001B,
+    0x001C,
+    0x001D,
+    0x001E,
+    0x001F,
+    0x0020,
+    0x0021,
+    0x0022,
+    0x0023,
+    0x0024,
+    0x0025,
+    0x0026,
+    0x0027,
+    0x0028,
+    0x0029,
+    0x002A,
+    0x002B,
+    0x002C,
+    0x002D,
+    0x002E,
+    0x002F,
+    0x0030,
+    0x0031,
+    0x0032,
+    0x0033,
+    0x0034,
+    0x0035,
+    0x0036,
+    0x0037,
+    0x0038,
+    0x0039,
+    0x003A,
+    0x003C,
+    0x003D,
+    0x003E,
+    0x003F,
+    0x0040,
+    0x0041,
+    0x0042,
+    0x0043,
+    0x0044,
+    0x0045,
+    0x0046,
+    0x0047,
+    0x0048,
+    0x0049,
+    0x004A,
+    0x004B,
+    0x004C,
+    0x004D,
+    0x004E,
+    0x004F,
+    0x0050,
+    0x0052,
+    0x0053,
+    0x0054,
+    0x0055,
+    0x0056,
+    0x0057,
+    0x0058,
+    0x0059,
+    0x005A,
+    0x005B,
+    0x005C,
+    0x005D,
+    0x005E,
+    0x005F,
+    0x0060,
+    0x0061,
+    0x0062,
+    0x0063,
+    0x0064,
+    0x0065,
+    0x0066,
+    0x0067,
+    0x0068,
+    0x0069,
+    0x006A,
+    0x006B,
+    0x006C,
+    0x006D,
+    0x006E,
+    0x006F,
+    0x0070,
+    0x0071,
+    0x0072,
+    0x0073,
+    0x0074,
+    0x0075,
+    0x0076,
+    0x0077,
+    0x0078,
+    0x0079,
+    0x007A,
+    0x007B,
+    0x007C,
+    0x007D,
+    0x007E,
+    0x007F,
+    0x0080,
+    0x0081,
+    0x0082,
+    0x0083,
+    0x0084,
+    0x0085,
+    0x0089,
+    0x008A,
+    0x008B,
+    0x008C,
+    0x008D,
+    0x008E,
+    0x008F,
+    0x0090,
+    0x0091,
+    0x0092,
+    0x0093,
+    0x0094,
+    0x0095,
+    0x0097,
+    0x0098,
+    0x0099,
+    0x009A,
+    0x009B,
+    0x009C,
+    0x009D,
+    0x009E,
+    0x009F,
+    0x00A0,
+    0x00A1,
+    0x00A2,
+    0x00A3,
+    0x00A4,
+    0x00A5,
+    0x00A6,
+    0x00A7,
+    0x00A8,
+    0x00A9,
+    0x00AA,
+    0x00AB,
+    0x00AC,
+    0x00AD,
+    0x00AE,
+    0x00AF,
+    0x00B0,
+    0x00B1,
+    0x00B2,
+    0x00B3,
+    0x00B4,
+    0x00B5,
+    0x00B6,
+    0x00B7,
+    0x00B8,
+    0x00B9,
+    0x00BA,
+    0x00BB,
+    0x00BC,
+    0x00BD,
+    0x00BE,
+    0x00BF,
+    0x00C0,
+    0x00C1,
+    0x00C2,
+    0x00C3,
+    0x00C4,
+    0x00C5,
+    0x00C6,
+    0x00C7,
+    0x00C8,
+    0x00CA,
+    0x00CB,
+    0x00CC,
+    0x00CD,
+    0x00CE,
+    0x00CF,
+    0x00D0,
+    0x00D1,
+    0x00D2,
+    0x00D3,
+    0x00D4,
+    0x00D5,
+    0x00D6,
+    0x00D7,
+    0x00D8,
+    0x00D9,
+    0x00DA,
+    0x00DB,
+    0x00DD,
+    0x00DE,
+    0x00DF,
+    0x00E0,
+    0x00E1,
+    0x00E2,
+    0x00E7,
+    0x00E8,
+    0x00E9,
+    0x00EA,
+    0x00EB,
+    0x00EC,
+    0x00ED,
+    0x00EE,
+    0x00EF,
+    0x00F0,
+    0x00F1,
+    0x00F2,
+    0x00F3,
+    0x00F4,
+    0x00F5,
+    0x00F6,
+    0x00F7,
+    0x00F8,
+    0x00F9,
+    0x00FA,
+    0x00FB,
+    0x00FB,
+    0x00FC,
+    0x00FD,
+    0x00FE,
+    0x00FF,
+    0x0100,
+    0x0101,
+    0x0102,
+    0x0103,
+    0x0105,
+    0x0106,
+    0x0108,
+    0x0109,
+    0x010A,
+    0x010B,
+    0x010C,
+    0x010D,
+    0x010E,
+    0x0110,
+    0x0111,
+    0x0112,
+    0x0113,
+    0x0114,
+    0x0115,
+    0x0116,
+    0x0117,
+    0x0118,
+    0x0119,
+    0x011A,
+    0x011B,
+    0x011C,
+    0x011D,
+    0x011E,
+    0x011F,
+    0x0120,
+    0x0121,
+    0x0122,
+    0x0123,
+    0x0124,
+    0x0125,
+    0x0250,
+    0x0251,
+    0x0252,
+    0x0253,
+    0x0254,
+    0x0255,
+    0x0256,
+    0x0257,
+    0x0258,
+    0x0259,
+    0x025A,
+    0x025B,
+    0x025C,
+    0x025D,
+    0x025E,
+    0x025F,
+    0x0263,
+    0x0269,
+};
+
+
 #define MAKE_DSDR_PHY_REG(addr, data)          ((1<<31) | (((addr) & 0xff) << 16) | (((data) & 0xff)))
 
 //
@@ -791,6 +1086,9 @@ static int dsdr_gth_control(lldev_t dev, uint16_t addr, uint16_t data)
     res = res ? res : lowlevel_reg_wr32(dev, 0, REG_CFG_PHY_0, MAKE_DSDR_PHY_REG(addr, data));
     return res;
 }
+
+#if 0
+#define MAKE_DSDR_DRP_CMD(wr, idx, addr, data) ((((wr) & 1) << 28) | (((idx) ? 2 : 1) << 29) | (((addr) & 0xfff) << 16) | ((data) & 0xffff))
 
 static int dsdr_drp_reg_wr(lldev_t dev, unsigned port, uint16_t addr, uint16_t data)
 {
@@ -810,14 +1108,58 @@ static int dsdr_drp_reg_rd(lldev_t dev, unsigned port, uint16_t addr, uint16_t *
     *odata = val >> (port ? 16 : 0);
     return res;
 }
+#endif
+
+#define MAKE_DSDR_DRP_CMD(wr, idx, addr, data) ((((wr) & 1) << 30) | (((idx) & 0xf) << 26) | (((addr) & 0x3ff) << 16) | ((data) & 0xffff))
+
+//localparam DRP_SE_BIT = 31;
+//localparam DRP_WR_BIT = 30;
+//localparam DRP_CH_BIT = 29;
+//localparam DRP_C2_BIT = 28;
+//localparam DRP_C1_BIT = 27;
+//localparam DRP_C0_BIT = 26;
+
+enum {
+    DRP_QPLL_IDX0 = 0,
+    DRP_QPLL_IDX1 = 1,
+    DRP_CHAN_IDX0 = 8,
+};
+
+static int dsdr_drp_reg_wr(lldev_t dev, unsigned port, uint16_t addr, uint16_t data)
+{
+    int res = 0;
+    res = res ? res : lowlevel_reg_wr32(dev, 0, REG_CFG_PHY_0, MAKE_DSDR_DRP_CMD(1, port, addr, data));
+    res = res ? res : usleep(10);
+    return res;
+}
+
+static int dsdr_drp_reg_rd(lldev_t dev, unsigned port, uint16_t addr, uint16_t *odata)
+{
+    int res = 0;
+    uint32_t val = 0, j;
+    res = res ? res : lowlevel_reg_wr32(dev, 0, REG_CFG_PHY_0, MAKE_DSDR_DRP_CMD(0, port, addr, 0));
+    for (j = 0; j < 100; j++) {
+        res = res ? res : usleep(10);
+        res = res ? res : lowlevel_reg_rd32(dev, 0, REG_CFG_PHY_0, &val);
+        if (!(val >> 31))
+            break;
+    }
+    *odata = val;
+    return res;
+}
+
 
 int gthcommon_init(dev_m2_dsdr_t *d)
 {
     uint16_t val = 0;
     int res = 0;
     for (unsigned i = 0; i < SIZEOF_ARRAY(s_gth_usp_regs); i++) {
-        res = res ? res : dsdr_drp_reg_rd(d->base.dev, 0, s_gth_usp_regs[i], &val);
-        USDR_LOG("GTHC", USDR_LOG_WARNING, "GRP[%04x] => %04x\n", s_gth_usp_regs[i], val);
+        res = res ? res : dsdr_drp_reg_rd(d->base.dev, DRP_QPLL_IDX0, s_gth_usp_regs[i], &val);
+        USDR_LOG("GTHQ", USDR_LOG_WARNING, "GTQ[%04x] => %04x\n", s_gth_usp_regs[i], val);
+    }
+    for (unsigned i = 0; i < SIZEOF_ARRAY(s_gth_usp_chan_regs); i++) {
+        res = res ? res : dsdr_drp_reg_rd(d->base.dev, DRP_CHAN_IDX0, s_gth_usp_chan_regs[i], &val);
+        USDR_LOG("GTHC", USDR_LOG_WARNING, "GTC[%04x] => %04x\n", s_gth_usp_chan_regs[i], val);
     }
     return res;
 }
@@ -1754,8 +2096,8 @@ static int usdr_jesd204b_bringup_post(struct dev_m2_dsdr *dd)
 
     res = res ? res : dev_gpo_set(dev, IGPO_TIAFE_RX_SYNC_RESET, 0);
     res = res ? res : usleep(10000);
-    res = res ? res : dsdr_gth_control(dev, GTH_CTR_RX_EN, dd->hw_fpga_jesd_rx_en);
-    res = res ? res : dsdr_gth_control(dev, GTH_CTR_TX_EN, dd->hw_fpga_jesd_tx_en);
+  //  res = res ? res : dsdr_gth_control(dev, GTH_CTR_RX_EN, dd->hw_fpga_jesd_rx_en);
+  //  res = res ? res : dsdr_gth_control(dev, GTH_CTR_TX_EN, dd->hw_fpga_jesd_tx_en);
 
     res = res ? res : dev_gpi_get32(dev, IGPI_JESD_SYSREF_RAC, &d);
     USDR_LOG("DSDR", USDR_LOG_ERROR, "STAT = %08x\n", d);
@@ -1792,8 +2134,8 @@ static int dsdr_gth_set_rate(struct dev_m2_dsdr *d, unsigned rate)
     }
 
     for (unsigned i = 0; i < SIZEOF_ARRAY(s_gth_qpll_ovrd_369); i += 2) {
-        res = res ? res : dsdr_drp_reg_wr(d->base.dev, 0, mode[i + 0], mode[i + 1]);
-        res = res ? res : dsdr_drp_reg_wr(d->base.dev, 1, mode[i + 0], mode[i + 1]);
+        res = res ? res : dsdr_drp_reg_wr(d->base.dev, DRP_QPLL_IDX0, mode[i + 0], mode[i + 1]);
+        res = res ? res : dsdr_drp_reg_wr(d->base.dev, DRP_QPLL_IDX1, mode[i + 0], mode[i + 1]);
     }
     return res;
 }
@@ -1823,8 +2165,8 @@ struct jesd_config
     unsigned logic_chcnt_tx;
     unsigned hw_chcnt_rx;
     unsigned hw_chcnt_tx;
-    unsigned hw_fpga_jesd_tx_en;
     unsigned hw_fpga_jesd_rx_en;
+    unsigned hw_fpga_jesd_tx_en;
 
     const channel_map_info_t *rx_chmap;
     const channel_map_info_t *tx_chmap;
@@ -1852,14 +2194,14 @@ struct jesd_config s_hwjesd_config[] = {
 
 
     { MODE_JESD204C | AFE_COMPAT_7950 | FPGA_COMPAT_8X_4X | FPGA_COMPAT_8X_8X,
-    RATE_491_52, 8, 4, 6, 4, 0xff, 0x0f, s_dsdr_chmap_s_nco_fb, s_dsdr_chmap_s_nco, s_dsdr_lmap_s_nco_fb, s_dsdr_lmap_s_nco, "4RX_4TX_2FB_491.52", "Afe79xxPg1_6664_491_6x1_4x1_20.txt", "Afe79xxPg1_6664_491_6x1_4x1_13.txt" },
+    RATE_491_52, 8, 4, 6, 4, 0x03f, 0x0f, s_dsdr_chmap_s_nco_fb, s_dsdr_chmap_s_nco, s_dsdr_lmap_s_nco_fb, s_dsdr_lmap_s_nco, "4RX_4TX_2FB_491.52", "Afe79xxPg1_6664_491_6x1_4x1_20.txt", "Afe79xxPg1_6664_491_6x1_4x1_13.txt" },
      //RATE_491_52, 6, 4, 6, 4, 0x3f, 0x0f, s_dsdr_chmap_s_nco_fb, s_dsdr_chmap_s_nco, s_dsdr_lmap_s_nco_fb, s_dsdr_lmap_s_nco, "4RX_4TX_2FB_491.52", NULL, "Afe79xxPg1_6664_2FB_491_7950.txt" },
 
     { MODE_JESD204C | AFE_COMPAT_7950 | FPGA_COMPAT_8X_4X | FPGA_COMPAT_8X_8X,
-     RATE_368_64, 6, 4, 4, 4, 0xff, 0x0f, s_dsdr_chmap_s_nco_rxfb, s_dsdr_chmap_s_nco, s_dsdr_lmap_s_nco_fbrx, s_dsdr_lmap_s_nco, "4RX2FBRB_4TX_368.64", "Afe79xxPg1_6664_368_6x1FBRX_4x1_20.txt", "Afe79xxPg1_6664_368_6x1FBRX_4x1_13.txt" },
+     RATE_368_64, 8, 4, 4, 4, 0x3f, 0x0f, s_dsdr_chmap_s_nco_rxfb, s_dsdr_chmap_s_nco, s_dsdr_lmap_s_nco_fbrx, s_dsdr_lmap_s_nco, "4RX2FBRB_4TX_368.64", "Afe79xxPg1_6664_368_6x1FBRX_4x1_20.txt", "Afe79xxPg1_6664_368_6x1FBRX_4x1_13.txt" },
 
     { MODE_JESD204C | AFE_COMPAT_7950 | FPGA_COMPAT_8X_4X | FPGA_COMPAT_8X_8X,
-     RATE_491_52, 8, 4, 6, 4, 0xff, 0x0f, s_dsdr_chmap_s_nco_rxfb, s_dsdr_chmap_s_nco, s_dsdr_lmap_s_nco_fbrx, s_dsdr_lmap_s_nco, "4RX2FBRB_4TX_491.52", "Afe79xxPg1_6664_491_6x1FBRX_4x1_20.txt", "Afe79xxPg1_6664_491_6x1FBRX_4x1_13.txt" },
+     RATE_491_52, 8, 4, 4, 4, 0x3f, 0x0f, s_dsdr_chmap_s_nco_rxfb, s_dsdr_chmap_s_nco, s_dsdr_lmap_s_nco_fbrx, s_dsdr_lmap_s_nco, "4RX2FBRB_4TX_491.52", "Afe79xxPg1_6664_491_6x1FBRX_4x1_20.txt", "Afe79xxPg1_6664_491_6x1FBRX_4x1_13.txt" },
 
 };
 
@@ -2752,7 +3094,7 @@ int usdr_device_m2_dsdr_create_stream(device_t* dev, const char* sid, const char
         }
 
         // TODO: set actual antenna mask
-        res = (res) ? res : dev_gpo_set(d->base.dev, IGPO_RX_CHEN, 0xf);
+        res = (res) ? res : dev_gpo_set(d->base.dev, IGPO_RX_CHEN, 0x3f); // d->hw_enabled_rx
         //res = (res) ? res : dev_gpo_set(d->base.dev, IGPO_RX_CHEN, 0x0);
 
         *out_handle = d->rx;
