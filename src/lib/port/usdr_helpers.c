9b39b4b2ef4ab959a82ee32ae30df11631a6f5ec
sergey <sergey.kostanbaev@gmail.com>
2026-02-05 14:10:50 +0400

    use ftell_long for cross platform 64-bit offset access

--- src/lib/port/usdr_helpers.c
+++ src/lib/port/usdr_helpers.c
@@ -11,7 +11,7 @@
 void* usdr_read_file(const char* filename, size_t* len)
 {
     char *buffer = NULL;
-    size_t length;
+    off_long_t length;
     FILE * f = fopen(filename, "rb");
     if (!f) {
         int err = errno;
@@ -23,15 +23,22 @@ void* usdr_read_file(const char* filename, size_t* len)
     }
 
     fseek(f, 0, SEEK_END);
-    length = ftell(f);
+    length = ftell_long(f);
     fseek(f, 0, SEEK_SET);
+
+    if (length > INTPTR_MAX) {
+        fclose(f);
+        USDR_LOG("FILE", USDR_LOG_CRITICAL_WARNING, "File is too big!\n");
+        return NULL;
+    }
+
     buffer = (char*)malloc(length);
     if (buffer) {
         size_t rd = fread(buffer, 1, length, f);
 
         USDR_LOG("FILE", USDR_LOG_DEBUG,
-                 "File %s was read %zu bytes\n",
-                 filename, rd);
+                 "File %s was read %llu bytes\n",
+                 filename, (long long)rd);
         *len = rd;
     }
     fclose(f);
